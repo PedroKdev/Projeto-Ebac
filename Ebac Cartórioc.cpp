1ac8a41ebd795997027e163a76b868034864d772
PedroKdev <167471563+PedroKdev@users.noreply.github.com>
2024-04-18 17:45:03 -0300

    Opção de saída
    incluso a opção de saída no sistema para o usuário.

--- Ebac Cartórioc.cpp
+++ Ebac Cartórioc.cpp
@@ -122,6 +122,7 @@ int main ()
     printf ("\t1 - Registrar Nomes\n");
     printf ("\t2 - Consultar Nomes\n");
     printf ("\t3 - Deletar Nomes\n\n");
+    printf ("\t4 - Sair do Sistema\n\n");
     printf ("Opção:")
     
    ;scanf("%d", &opcao) //guardando a informação do usuário
@@ -141,6 +142,11 @@ int main ()
 		
 		case 3:
 		deletar (); //chamando
+   		break;
+   		
+   		case 4:
+   		printf ("obrigado por utilizar o sistema\n");
+   		return 0;
    		break;
    		
    		default:
